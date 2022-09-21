db397de7f24bc265635fdc8c53fd2d37778e23a7
BT_4N2_06 <davidn166@nycstudents.net>
2022-09-21 12:13:37 -0400

    Initial commit

---
+++ src/Unit1Project.java
@@ -0,0 +1,26 @@
+import  java.text.DecimalFormat;
+import java.util.Scanner;
+public class Unit1Project {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("###.00");
+        System.out.println("Hello user.");
+        System.out.print("Please enter your Bill Here: $");
+        double bill = s.nextDouble();
+        System.out.print("Please enter your Tip Percentage Here: %");
+        double tipPercentage = s.nextDouble();
+        System.out.print("Please enter your Party Amount Here: ");
+        int totalPeople = s.nextInt();
+        double tipTotal = (bill / 100) * tipPercentage;
+                double tipPerPerson = tipTotal / totalPeople;
+                double totalBill = tipTotal + bill;
+                double totalPerPerson = totalBill / totalPeople;
+        System.out.println("-----------------------------------");
+        System.out.println("");
+        System.out.println("Your Total Tip is: $" + (df.format(Math.round(tipTotal*100.0)/100.0)));
+        System.out.println("Your Tip Per Person is: $" + (df.format(Math.round(tipPerPerson*100.0)/100.0)));
+        System.out.println("Your Total Per Person is: $" + (df.format(Math.round(totalPerPerson*100.0)/100.0)));
+        System.out.println("Your Total Bill is: $" + (df.format(Math.round(totalBill*100.0)/100.0)));
+
+    }
+}
